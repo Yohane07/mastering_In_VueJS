4d5b99236687147dea11a542d707e007f29a83dd
ENDOKE Yohane <yohane.endoke@soprasteria.com>
2024-01-21 06:57:27 +0100

    Rourer link done

---
+++ src/components/NavLink.vue
@@ -0,0 +1,17 @@
+<template>
+    <a href="url">{{ text }}</a> <span>{{ separateur }}</span> <a href="url2">{{ text2 }}</a>
+</template>
+
+<script>
+export default {
+    name: 'NavLink',
+    props:['url', 'text', 'separateur', 'url2', 'text2']
+    }
+</script>
+
+<style>
+a{
+    text-decoration: none;
+    color: #42b983;
+}
+</style>
